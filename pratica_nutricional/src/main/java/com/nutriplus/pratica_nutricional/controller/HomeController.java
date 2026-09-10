3a3dd82c3778d8e952a6faa480044dc896b740f5
Lucas Damacena <lucas.o.damacena@gmail.com>
2026-09-10 15:36:11 -0300

    feat: added controller for testing http requests, added dockerfile and docker compose

---
+++ pratica_nutricional/src/main/java/com/nutriplus/pratica_nutricional/controller/HomeController.java
@@ -0,0 +1,16 @@
+
+package com.nutriplus.controle_nutricional.controller; 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    String service_name = "pratica_nutricional";
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return String.format("O serviço %s está rodando", service_name);
+    }
+}
